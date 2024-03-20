0d43560987da0782e6964f3054c3b3490cee0170
amrodrigues <42442977+amrodrigues@users.noreply.github.com>
2024-03-19 21:07:58 -0300

    Merge branch 'exercicio em aula 20240318'

--- exercicio_aula/AtividadeListadeTarefas.java
+++ exercicio_aula/AtividadeListadeTarefas.java
@@ -3,69 +3,75 @@ package exercicio_aula;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class AtividadeListadeTarefas {
-    public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
-        ArrayList<String> listadetarefas = new ArrayList<>();
-        String tarefa = "";
-        int opcao = 0 , indice ;
+
+    public static void menu (){
         System.out.println("Escolha o tipo de condição da tarefa");
         System.out.println("1-Adicionar");
         System.out.println("2-Excluir");
         System.out.println("3-Mostrar");
         System.out.println("4-Sair");
+    }
 
+    public static void listarTarefa( ArrayList<String> listadetarefas){
+        System.out.println("---- Lista de Tarefas ---");
+        System.out.println(listadetarefas);
+        System.out.println("------------------------");
+
+    }
+
+    public static void adicionarTarefa(ArrayList<String> listadetarefas , Scanner leitura, String tarefa) {
+        System.out.println("Digite o nome da tarefa para adicionar:");
+        tarefa = leitura.nextLine();
+        listadetarefas.add(tarefa);
+    }
+
+  public static void excluirTarefa(ArrayList<String> listadetarefas , Scanner leitura, String tarefa, int indice)
+    {
+        System.out.println("Digite parte do nome da tarefa para excluir:");
+        tarefa = leitura.nextLine();
+        indice =0;
+        for (int i = 0; i < listadetarefas.size(); i++) {
+            if (listadetarefas.get(i).contains(tarefa)) {
+                System.out.println("Entrei aqui");
+                indice = i;
+                listadetarefas.remove(indice);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        ArrayList<String> listadetarefas = new ArrayList<>();
+        String tarefa = "";
+        int opcao = 0 , indice=0 ;
+
+        menu();
         opcao = leitura.nextInt();
         leitura.nextLine();
         while (opcao != 4) {
             switch (opcao) {
                 case 1:
-                    System.out.println("Digite o nome da tarefa para adicionar:");
-                    tarefa = leitura.nextLine();
-                    listadetarefas.add(tarefa);
+                    adicionarTarefa(listadetarefas , leitura ,tarefa);
                     break;
                 case 2:
-                    System.out.println("---- Lista de Tarefas ---");
-                    System.out.println(listadetarefas);
-                    System.out.println("------------------------");
-                    System.out.println("Digite parte do nome da tarefa para excluir:");
-                    tarefa = leitura.nextLine();
-                    for (int i = 0; i < listadetarefas.size(); i++) {
-                        if (listadetarefas.get(i).contains(tarefa)) {
-                            System.out.println("Entrei aqui");
-                            indice = i;
-                            listadetarefas.remove(indice);
-                        }
-                    }
+                    listarTarefa(listadetarefas);
 
-                   // for(int i = 0 ; i < listadetarefas.size() ; i++){
-                  //      System.out.println(" Indice "+ (i+1) + " Tarefa : "+ listadetarefas.get(i));
-                  //  }
-                 //   System.out.println("Digite o numero da tarefa que quer apagar");
-                 //   int apagar = leitura.nextInt();
-                //    listadetarefas.remove((apagar-1));
-                 //   System.out.println("Tarefa apagada com sucesso");
+                    excluirTarefa(listadetarefas, leitura , tarefa , indice) ;
 
-                    System.out.println("----Nova Lista de Tarefas ---");
-                    System.out.println(listadetarefas);
-                    System.out.println("------------------------");
+
+                    listarTarefa(listadetarefas);
 
                     break;
                 case 3:
 
-                    System.out.println("---- Lista de Tarefas ---");
-                    System.out.println(listadetarefas);
-                    System.out.println("------------------------");
+                    listarTarefa(listadetarefas);
 
                     break;
                 default:
                     System.out.println("Opcao Invalida");
             }
-            System.out.println("Escolha o tipo de condição da tarefa");
-            System.out.println("1-Adicionar");
-            System.out.println("2-Excluir");
-            System.out.println("3-Mostrar");
-            System.out.println("4-Sair");
+            menu();
             opcao = leitura.nextInt();
             leitura.nextLine();
 
